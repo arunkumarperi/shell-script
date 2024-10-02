2893ac0f5a40ee16f2dc109c69a9cee7db03ce55
arunkumar <arunkumarperipogu93@gmail.com>
2024-10-02 13:18:29 +0530

    data-types

---
+++ 20-monitoring.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+RAM=free -m |awk -F " " '{print $2,$3}'
+DISK=df -hT |grep xfs|awk -F " " '{print $7,$6}'
+CPU=top -bn1 |awk -F " " '{print $9}'
+
+end=$((SECONDS+3600))
+
+while [ $SECONDS -lt $end ]; 
+do
+echo "ram usage :$RAM"
+echo "disk usage :$DISK"
+echo "cpu usage :$CPU"
+
+sleep 5
+done
+
+
